5825a7b27adddfb2cc6ccd628a1e749f091d278a
Lil Hobby <gregbedford93@gmail.com>
2023-07-18 20:22:07 -0400

    Refactored Code

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
 $(function() {
-  //   TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-var saveBtn = $(".saveBtn");
-
-saveBtn.on( "click", function( ) {
-alert("saved!");
-});
-
 var today = dayjs().format("dddd[,] MMMM D[,] YYYY");
-$("#currentDay").html(today);
-
 var currentHour = dayjs().format("h");
+$("#currentDay").html(today);
 var i = 1;
-
 while (i<14) { 
-  
   var timeBlock = $("#hour-" + i); 
-
   if (currentHour >= 6 && currentHour < 9){
     timeBlock.addClass("future");
   } else {
@@ -45,16 +28,18 @@ while (i<14) {
       timeBlock.addClass("future");
     }
   }
-
-  
   ++i;
 }
-
-    // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
-  // TODO: Add code to display the current date in the header of the page.
-
+//   TODO: Add a listener for click events on the save button. This code should
+// use the id in the containing time-block as a key to save the user input in
+// local storage. HINT: What does `this` reference in the click listener
+// function? How can DOM traversal be used to get the "hour-x" id of the
+// time-block containing the button that was clicked? How might the id be
+// useful when saving the description in local storage?
+$(".saveBtn").on( "click", function( ) {
+  alert("saved!");
+  });
+// TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id
+// attribute of each time-block be used to do this?
 });
